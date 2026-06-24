c65bb51aca0bc61874451025ea9efe2c3450e6d1
VicenteRen <vice.cespedes@duocuc.cl>
2026-06-24 18:03:56 -0400

    Agreo API Gateway y Eureka Server al repositorio

---
+++ eureka-server/src/test/java/cl/duoc/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
